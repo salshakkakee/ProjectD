0a3a5bb4caae33b0b9065d7519d8d34ad3badd9a
Sally <sally.ayad992@gmail.com>
2025-01-31 16:04:35 -0500

    Update DemoTestD.java

--- src/Demo1/DemoTestD.java
+++ src/Demo1/DemoTestD.java
@@ -3,7 +3,9 @@ package Demo1;
 public class DemoTestD {
 	public static void main(String[] args) {
 		int num[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int num[] = { 9, 9, 9, 9, 9};
 		int total = 0;
+		
 
 		for (int i = 0; i < num.length; i++) {
 
